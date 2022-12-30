139ca4b7ec110c8d360ec08a4784bd8340366d2e
只因只因 <117057486+Liulw123@users.noreply.github.com>
2022-12-30 16:53:58 +0800

    fuck , i fix again

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -84,8 +84,8 @@ class MainMenuState extends MusicBeatState
 		
 		var bffuckingdead:FlxSprite = new FlxSprite(FlxG.width * 0.4, FlxG.height * 0.07);
 		bffuckingdead.frames = Paths.getSparrowAtlas('characters/BOYFRIEND');
-		bffuckingdead.animation.addByPrefix('idle', 'BF idle dance', 24, true)
-		bffuckingdead.animation.play('idle')
+		bffuckingdead.animation.addByPrefix('idle', 'BF idle dance', 24, true);
+		bffuckingdead.animation.play('idle');
 		bffuckingdead.antialiasing = ClientPrefs.globalAntialiasing;
 		add(bffuckingdead);
 		
