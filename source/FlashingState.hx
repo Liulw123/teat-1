03a8347df2c5ca9b3a23a88ac74573f08c9d3f71
只因只因 <117057486+Liulw123@users.noreply.github.com>
2022-12-30 11:42:25 +0800

    yes test

--- source/FlashingState.hx
+++ source/FlashingState.hx
@@ -22,12 +22,14 @@ class FlashingState extends MusicBeatState
 
 		var bg:FlxSprite = new FlxSprite().makeGraphic(FlxG.width, FlxG.height, FlxColor.BLACK);
 		add(bg);
+		
+		var redFont = new FlxTextFormatMarkerPair(new FlxTextFormat(FlxColor.RED, true, true), '^');
 
 		warnText = new FlxText(0, 0, FlxG.width,
-			"Hey, watch out!\n
-			This Mod contains some flashing lights!\n
-			Press ENTER to disable them now or go to Options Menu.\n
-			Press ESCAPE to ignore this message.\n
+			"'^Hey, watch out!^'\n
+			This engine maker is '^ye_nb^'\n
+			Press '^ENTER/A^' to disable them now or go to Options Menu.\n
+			Press '^ESCAPE/B^' to ignore this message.\n
 			You've been warned!",
 			32);
 		warnText.setFormat("VCR OSD Mono", 32, FlxColor.WHITE, CENTER);
