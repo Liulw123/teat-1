04412cdab7c61c7eb6715f4666eaefe7537989b7
只因只因 <117057486+Liulw123@users.noreply.github.com>
2023-01-01 11:56:38 +0800

    code

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -35,7 +35,7 @@ class MainMenuState extends MusicBeatState
 	var optionShit:Array<String> = [
 		'story_mode',
 		'freeplay',
-	    if !switch 'mods', #end
+	    // if !switch 'mods', #end
 		// if ACHIEVEMENTS_ALLOWED 'awards', #end
 		'credits',
 		// if !switch 'donate', #end
@@ -134,7 +134,7 @@ class MainMenuState extends MusicBeatState
 			menuItem.animation.addByPrefix('selected', optionShit[i] + " white", 24);
 			menuItem.animation.play('idle');
 			menuItem.ID = i;
-			menuItem.x = 700;
+			menuItem.x = 50;
 			menuItems.add(menuItem);
 			var scr:Float = (optionShit.length - 4) * 0.135;
 			if(optionShit.length < 6) scr = 0;
